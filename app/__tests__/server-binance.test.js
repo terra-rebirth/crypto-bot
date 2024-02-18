541c9999d712cdf984dc9e1150e07f8025f02dcf
chaiya srisawat <chaiya.88488@gmail.com>
2024-02-18 13:48:57 +0700

    .

--- app/__tests__/server-binance.test.js
+++ app/__tests__/server-binance.test.js
@@ -33,9 +33,14 @@ describe('server-binance', () => {
   let mockSlack;
   let config;
 
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => { });
+
   beforeEach(async () => {
     jest.clearAllMocks().resetModules();
-    jest.useFakeTimers();
 
     jest.mock('config');
     jest.mock('../cronjob');
